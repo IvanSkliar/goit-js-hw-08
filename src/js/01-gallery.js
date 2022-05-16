4aebdea01ec105bd8e066764ecd90378d1c671e0
IvanSkliar <skliariv91@gmail.com>
2022-05-16 22:34:06 +0300

    dz-8-45

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const imgContainer = document.querySelector('.gallery');
+
+
+
+const cardsMarkup = createImgCards(galleryItems);  
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+    }).join('');
+}
+
+
+const gallery = new SimpleLightbox('.gallery a');
+
+gallery.options.captionsData = "alt";
+gallery.options.captionDelay = 250;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+const throttle = require("lodash.throttle");
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea'),
+};
+const KEY_STORAGE = 'feedback-form-state'; 
+
+refs.form.addEventListener("input", throttle(onWriteInfToStorage, 500));
+refs.form.addEventListener("submit", onSubmitForm);
+onCheckStorage();
+
+function onSubmitForm(evt) {
+     const value = {};
+    value.email = refs.email.value;
+    value.message = refs.message.value;
+    evt.preventDefault();
+    refs.form.reset();
+    localStorage.removeItem(KEY_STORAGE);
+    console.log(value);
+}
+
+function onWriteInfToStorage() {
+    const value = {};
+    value.email = refs.email.value;
+    value.message = refs.message.value;
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(value));
+    
+    
+};
+
+function onCheckStorage() {
+    const dataForm = localStorage.getItem(KEY_STORAGE);
+
+    if (dataForm) {
+        const dataObject = JSON.parse(dataForm);
+        refs.email.value = dataObject.email;
+        refs.message.value = dataObject.message;
+        
+    };
+
+};

e853176b710ec5e3be7050f2ff62acff843123f4
IvanSkliar <skliariv91@gmail.com>
2022-05-20 11:14:46 +0300

    dz8/16

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,33 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
-const galleryEl = document.querySelector('.gallery');
 
-galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup(galleryItems));
+const galleryRef = document.querySelector('.gallery');
 
-import galleryItemMarkup from "./lightbox";
+// ------------Создать галлерею------------
 
-let lightbox = new SimpleLightbox('.gallery__link', {captionDelay: 250});
+function createGalleryCard(galleryRef) {
+    return galleryItems.map(galleryItem => {
+       galleryRef.insertAdjacentHTML('beforeend', `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/> 
+</a>`);
+    });
+}
+console.log(createGalleryCard(galleryRef));
+
+// ---------------Создание simplelightbox---------------
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+  captionsData: 'alt',});
+    gallery.on('show.simplelightbox');
+
 console.log(galleryItems);
+
+export default {
+  galleryRef,
+  gallery,
+}
